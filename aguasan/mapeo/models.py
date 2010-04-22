867264a1f672d0ab99cc6e233153e1ce6b354dcd
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2010-04-22 17:31:24 +0000

    adding apps, settings

---
+++ aguasan/mapeo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from lugar.models import Municipio
+
+class Proyecto(models.Model):
+    pass
+
+class Donante (models.Model):
+    pass
+
+class ProyectoMunicipio(models.Model):
+    pass
+
+class ProyectoDonante(models.Model):
+    pass
