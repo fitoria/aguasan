c8994e9468b75cef4b298d0d89fc33012f90f415
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2010-05-13 23:59:27 -0600

    fixing class issue

--- aguasan/mapeo/forms.py
+++ aguasan/mapeo/forms.py
@@ -1,9 +1,17 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput
 from models import *
 
 class ProyectoForm(ModelForm):
     class Meta:
         model = Proyecto
+        widgets = {
+            'descripcion': Textarea(attrs={'class': 'textarea'}),
+            'nombre': TextInput(attrs={'class': 'textbox_nombre'}),
+            'website': TextInput(attrs={'class': 'textbox'}),
+            'fecha_inicial': TextInput(attrs={'class': 'textbox hasDatepicker'}),
+            'fecha_final': TextInput(attrs={'class': 'textbox hasDatepicker'}),
+            #TODO: ponerle bien la class a los combobox
+        }
 
 class ProyectoDepartamentoForm(ModelForm):
     class Meta:
