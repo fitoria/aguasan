491095c64c8acb5fa2a4ea125c3680ec478f5d0a
bcorrales <bcorrales@bcorrales-laptop.(none)>
2010-05-25 17:23:08 -0600

    siguiendo con los mapas magicos

--- aguasan/mapeo/views.py
+++ aguasan/mapeo/views.py
@@ -423,6 +423,7 @@ def proyectos_donante(request, id_donante):
     for proyecto_donante in _proyectos_donante:
         proyectos.append(proyecto_donante.proyecto)
     donante=Donante.objects.get(id=id_donante)
+    
     return render_to_response('mapeo/proyectos_donante.html',{'donante':donante,'proyectos':proyectos},
                               context_instance=RequestContext(request))
 
